8b65574164a9944128f582a5ee48d44ea5eb50b3
Kostas <kostas.sotiriadis@gmail.com>
2015-12-23 18:35:17 +0000

    1st commit attempt

---
+++ Test1/src/test1/Test.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		new Test().temp1();
+	}
+
+	private void temp1() {
+		// TODO: Test to-do note
+		System.out.println("@@@");
+	}
+
+}
